ab88404da6b3479969e9889baca73cace45260b2
Estrela <estrela.steinkirch@gmail.com>
2020-11-17 17:28:43 +0100

    add Component Period

--- app/client/src/components/Period.js
+++ app/client/src/components/Period.js
@@ -1,12 +1,46 @@
-import React, { useState } from "react";
-import DecrementButton from "./DecrementButton.js";
-import IncrementButton from "./IncrementButton.js";
-import yearMonth from "../options.js"
+import React, { useEffect, useState } from "react";
+import yearMonth from "../options.js";
+import M from "materialize-css";
+import Header from "./Header.js";
+import transactionsService from "../services/transactionsService.js";
+import Transactions from "./Transactions.js";
+import Panel from "./Panel.js";
+import Filter from "./Filter.js";
+import ModalNew from "./ModalNew.js";
+import ModalEdit from "./ModalEdit.js";
 
-export default function Period(props) {
-  const [currentDate, setCurrentDate] = useState("2019-11");
+export default function Period() {
+  const [currentDate, setCurrentDate] = useState("2020-07");
   const [disabledIncrement, setDisabledIncrement] = useState(false);
   const [disabledDecrement, setDisabledDecrement] = useState(false);
+  const [transactionsPeriod, setTransactionsPeriod] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const filterLowerCase = filter.toLowerCase();
+    const filteredTransactions = transactionsPeriod.filter((t) => {
+      return t.description.includes(filterLowerCase);
+    });
+    setFilteredTransactions(filteredTransactions);
+  }, [filter, transactionsPeriod]);
+
+  useEffect(() => {
+    const getTransactions = async (period) => {
+      const response = await transactionsService.getPeriod(period);
+      const allTransactions = response.data;
+      setTransactionsPeriod(allTransactions);
+      setFilteredTransactions(Object.assign([], allTransactions));
+    };
+
+    getTransactions(currentDate);
+    M.AutoInit();
+  }, [currentDate]);
+
+  const handleChangeFilter = (event) => {
+    const { value } = event.target;
+    setFilter(value);
+  };
 
   const handleOnChangeDate = (event) => {
     const { value } = event.target;
@@ -17,21 +51,16 @@ export default function Period(props) {
   const handleNextDate = (clickType) => {
     //pega o índice que está a data atual:
     let currentIndex = yearMonth.findIndex((obj) => obj.value === currentDate);
-
     //verifica se apertou > ou < e soma ou subtrai o index
     let nextIndex = clickType === ">" ? currentIndex + 1 : currentIndex - 1;
-    console.log(nextIndex);
-
     let nextDate;
-
     if (nextIndex >= yearMonth.length) {
       nextDate = yearMonth[currentIndex];
       setDisabledIncrement(true);
-    } else if(nextIndex < 0){
+    } else if (nextIndex < 0) {
       nextDate = yearMonth[currentIndex];
       setDisabledDecrement(true);
-    }
-      else {
+    } else {
       setDisabledIncrement(false);
       setDisabledDecrement(false);
       //acha a posição referente ao index:
@@ -39,32 +68,22 @@ export default function Period(props) {
       //mudar o valor no select:
       setCurrentDate(nextDate.value);
     }
-
   };
 
   return (
     <div>
-      
-      <DecrementButton onDecrement={handleNextDate} disabled={disabledDecrement}/>
-      <div className="input-field col s6">
-        <select
-          value={currentDate}
-          onChange={handleOnChangeDate}
-          className="browser-default "
-        >
-          {yearMonth.map(({ value, display }, index) => {
-            return (
-              <option key={index} value={value} posicao={index + 1}>
-                {display}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <IncrementButton
-        onIncrement={handleNextDate}
-        disabled={disabledIncrement}
+      <Header
+        disabledDecrement={disabledDecrement}
+        disabledIncrement={disabledIncrement}
+        onChangeDate={handleOnChangeDate}
+        onNextDate={handleNextDate}
+        date={currentDate}
       />
+      <Panel transactions={filteredTransactions} />
+      <ModalNew />
+      <Filter filter={filter} onChangeFilter={handleChangeFilter} />
+      <Transactions transactions={filteredTransactions} />
+      {/* <ModalEdit /> */}
     </div>
   );
 }
