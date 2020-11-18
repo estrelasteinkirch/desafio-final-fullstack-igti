10cb58fe01f2cb89d91f0e069261d9fe2ef5c459
Estrela <estrela.steinkirch@gmail.com>
2020-11-18 23:17:37 +0100

    remove localhost url

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: "http://localhost:3001/api/transaction",
+  baseURL: "/api/transaction",
   headers: {
     "Content-type": "application/json",
   },
